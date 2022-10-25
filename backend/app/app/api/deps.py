c971f4ae611de963a3e4d56bea9e93185d668953
l2m2 <l2m2lq@gmail.com>
2022-10-25 16:57:29 +0800

    chore: format code

--- backend/app/app/api/deps.py
+++ backend/app/app/api/deps.py
@@ -27,10 +27,7 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(reusabl
     payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[security.ALGORITHM])
     token_data = schemas.TokenPayload(**payload)
   except (jwt.JWTError, ValidationError):
-    raise HTTPException(
-      status_code=status.HTTP_403_FORBIDDEN,
-      detail="Could not validate credentials",
-    )
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials")
   user = crud.user.get(db, id=token_data.sub)
   if not user:
     raise HTTPException(status_code=404, detail="User not found")
